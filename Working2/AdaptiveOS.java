4b06f62816bc262e85b718cdbf28be7b6542fc00
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2023-04-06 18:02:39 +0200

    Update AdaptiveOS.java

--- Working2/AdaptiveOS.java
+++ Working2/AdaptiveOS.java
@@ -1,7 +1,5 @@
 
 import java.util.Map;
-
-
 import genius.core.AgentID;
 import genius.core.Bid;
 import genius.core.actions.ActionWithBid;
@@ -11,7 +9,9 @@ import genius.core.boaframework.NegotiationSession;
 import genius.core.boaframework.OMStrategy;
 import genius.core.boaframework.OfferingStrategy;
 import genius.core.boaframework.OpponentModel;
+import genius.core.boaframework.SortedOutcomeSpace;
 import genius.core.utility.AdditiveUtilitySpace;
+import genius.core.BidHistory;
 
 public class AdaptiveOS extends OfferingStrategy {
 
@@ -58,53 +58,77 @@ public class AdaptiveOS extends OfferingStrategy {
 	@Override
 	public BidDetails determineNextBid() {
 		
-		
-			
-		    
-		simulator.evaluatePredictions(negotiationSession.getOpponentBidHistory().getLastBid());
-		
-		this.simulator.updateUtilitySpace(opponentModel.getOpponentUtilitySpace());
-		
-		try {
-			this.HHagent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
-		} catch (Exception e) {
-			
-		}
-		this.bramAgent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
-		this.tftAgent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
-		negotiationSession.getOpponentBidHistory().getHistory().remove(negotiationSession.getOpponentBidHistory().size() - 1);
-		//Get best Agent
-		
-	    simulator.evaluatePredictions(negotiationSession.getOpponentBidHistory().getLastBid());
-	    String bestAgent = simulator.getBestAgent();
-	    
 		Bid nextBid = null;
-		
-		
-		
-		
-		ActionWithBid hhAction = (ActionWithBid) this.HHagent.chooseAction();
-		ActionWithBid bramAction = (ActionWithBid) this.bramAgent.chooseAction();
-		ActionWithBid tftAction = (ActionWithBid) this.tftAgent.chooseAction();
-		
-		HHagent.setLastAction(hhAction);
-		bramAgent.setLastAction(bramAction);
-		tftAgent.setLastAction(tftAction);
-		
-		
-		if (bestAgent.equals("Bram")) {
-			nextBid = hhAction.getBid();
-		}else if (bestAgent.equals("HH")) {
-			nextBid = bramAction.getBid();
-		}else if (bestAgent.equals("TFT")) {
-			nextBid = bramAction.getBid();
+		if (this.negotiationSession.getTime() < 0.95 ) {
+			simulator.evaluatePredictions(negotiationSession.getOpponentBidHistory().getLastBid());
+			
+			this.simulator.updateUtilitySpace(opponentModel.getOpponentUtilitySpace());
+			
+			try {
+				this.HHagent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
+			} catch (Exception e) {
+				
+			}
+			this.bramAgent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
+			this.tftAgent.ReceiveMessage(new Offer(new AgentID("simu"), negotiationSession.getOpponentBidHistory().getLastBid()));
+			negotiationSession.getOpponentBidHistory().getHistory().remove(negotiationSession.getOpponentBidHistory().size() - 1);
+			//Get best Agent
+			
+		    simulator.evaluatePredictions(negotiationSession.getOpponentBidHistory().getLastBid());
+		    String bestAgent = simulator.getBestAgent();
+		    
+			
+			
+			
+			
+			
+			ActionWithBid hhAction = (ActionWithBid) this.HHagent.chooseAction();
+			ActionWithBid bramAction = (ActionWithBid) this.bramAgent.chooseAction();
+			ActionWithBid tftAction = (ActionWithBid) this.tftAgent.chooseAction();
+			
+			HHagent.setLastAction(hhAction);
+			bramAgent.setLastAction(bramAction);
+			tftAgent.setLastAction(tftAction);
+			
+			
+			if (bestAgent.equals("Bram")) {
+				nextBid = hhAction.getBid();
+			}else if (bestAgent.equals("HH")) {
+				nextBid = bramAction.getBid();
+			}else if (bestAgent.equals("TFT")) {
+				nextBid = bramAction.getBid();
+			}
+			
+			
+			simulator.receiveAction(new Offer(new AgentID("simu"), nextBid));
+		}else {
+			
+			double ownAverageUtility = getAvarageUtility(this.negotiationSession.getOwnBidHistory());
+			double opponentAverageUtility = getAvarageUtility(this.negotiationSession.getOpponentBidHistory());
+			double utilityGoal = (ownAverageUtility + opponentAverageUtility)/2;
+			nextBid = this.omStrategy.getBid(new SortedOutcomeSpace(this.negotiationSession.getUtilitySpace()), utilityGoal ).getBid();
+			
 		}
 		
-		
-		simulator.receiveAction(new Offer(new AgentID("simu"), nextBid));
 		return new BidDetails(nextBid, negotiationSession.getUtilitySpace().getUtility(nextBid));
 	}
-
+	
+	private double getAvarageUtility(BidHistory history) {
+		
+		double sum = 0;
+		for (BidDetails bidD : history.getHistory()) {
+			
+			Bid bid = bidD.getBid();
+			sum += this.negotiationSession.getUtilitySpace().getUtility(bid);
+			
+			
+			
+		}
+		
+		return sum/(this.negotiationSession.getOwnBidHistory().getHistory().size());
+		
+	}
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
